d17e653ed3fd1413c00f23a16ecf472a9b7c0050
Muhammad Arfan <arfan2173@gmail.com>
2025-04-24 13:27:50 +0700

    separated into hook

---
+++ src/layouts/Layout.tsx
@@ -0,0 +1,25 @@
+import { ReactNode } from "react";
+import useDocumentTitle from "@/hooks/useDocumentTitle";
+
+/**
+ * Layout component that sets the document title based on route match titles.
+ *
+ * @param {Object} props - The props object.
+ * @param {ReactNode} props.children - The child components to render.
+ *
+ * @returns {ReactNode} Rendered children components.
+ *
+ * This component uses the `useMatches` hook from `react-router` to get the
+ * current route matches and filter them for those with a title handle.
+ * It then sets the document title to the last matching route's title, followed
+ * by the application name. If no route has a title handle, the document title
+ * defaults to the application name.
+ */
+
+const Layout = ({ children }: { children: ReactNode }): ReactNode => {
+	useDocumentTitle();
+
+	return <>{children}</>;
+};
+
+export default Layout;
