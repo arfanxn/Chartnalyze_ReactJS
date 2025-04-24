9d3594831f55fe8c7b28374f2732e04bae569dab
Muhammad Arfan <arfan2173@gmail.com>
2025-04-24 15:52:31 +0700

    added unplugin icons

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import tailwindcss from "@tailwindcss/vite";
-import { fileURLToPath } from "url";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath } from 'url'
+import UnpluginIcons from 'unplugin-icons/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
-	resolve: {
-		alias: {
-			"@": fileURLToPath(new URL("./src", import.meta.url)),
-		},
-	},
-});
+    plugins: [react(), tailwindcss(), UnpluginIcons({ compiler: 'jsx' })],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+        },
+    },
+})
