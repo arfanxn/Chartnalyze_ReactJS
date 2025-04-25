137a075840a6adfb1cabf667e149a148b8aad142
Muhammad Arfan <arfan2173@gmail.com>
2025-04-25 08:59:18 +0700

    register a route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,19 +1,9 @@
-import Home from "@/features/home/pages/Home";
-import RootLayout from "@/layouts/RootLayout";
+import Landing from "@/pages/Landing";
 import { createBrowserRouter } from "react-router";
 
 export const router = createBrowserRouter([
 	{
 		path: "/",
-		Component: RootLayout,
-		handle: {
-			title: "Home",
-		},
-		children: [
-			{
-				index: true,
-				Component: Home,
-			},
-		],
+		Component: Landing,
 	},
 ]);
