babc93955f4d0e847d0363cb28b4053c2d393043
Muhammad Arfan <arfan2173@gmail.com>
2025-05-01 21:55:23 +0700

    added effect on hover and active

--- src/components/CButton.tsx
+++ src/components/CButton.tsx
@@ -11,7 +11,7 @@ export default function CButton(props: Props) {
     return (
         <button
             className={classNames(
-                'bg-primary rounded-md px-2 py-1 text-white md:px-4 md:py-2',
+                'bg-primary rounded-md px-2 py-1 text-white transition-transform duration-150 hover:scale-105 active:scale-105 md:px-4 md:py-2',
                 props.className,
             )}
             onClick={props.onClick}
