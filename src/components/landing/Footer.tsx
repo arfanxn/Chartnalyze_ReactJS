1f8b7d7bbfc51d2afff81506ae0368539799f81d
Muhammad Arfan <arfan2173@gmail.com>
2025-05-01 19:51:18 +0700

    simplify header and footer

--- src/components/landing/Footer.tsx
+++ src/components/landing/Footer.tsx
@@ -4,123 +4,123 @@ import classNames from 'classnames'
 import React, { forwardRef } from 'react'
 import moment from 'moment'
 
-const LandingFooter = forwardRef<
-    HTMLDivElement,
-    React.HTMLProps<HTMLDivElement>
->(({ className, ...props }, ref) => {
-    const appName = import.meta.env.VITE__APP_NAME
-    const appDescription = `Chartnalyze combines AI-powered market analysis with
+const Footer = forwardRef<HTMLDivElement, React.HTMLProps<HTMLDivElement>>(
+    ({ className, ...props }, ref) => {
+        const appName = import.meta.env.VITE__APP_NAME
+        const appDescription = `Chartnalyze combines AI-powered market analysis with
                     easy-to-understand financial education, helping both
                     beginners and experts make confident, data-driven investment
                     decisions in real-time.`
 
-    const navigations = [
-        {
-            label: 'Home',
-            url: '/',
-        },
-        {
-            label: 'Register',
-            url: '/register',
-        },
-        {
-            label: 'Login',
-            url: '/login',
-        },
-        {
-            label: 'Forgot Password',
-            url: '/forgot-password',
-        },
-    ]
+        const navigations = [
+            {
+                label: 'Home',
+                url: '/',
+            },
+            {
+                label: 'Register',
+                url: '/register',
+            },
+            {
+                label: 'Login',
+                url: '/login',
+            },
+            {
+                label: 'Forgot Password',
+                url: '/forgot-password',
+            },
+        ]
 
-    const downloads = [
-        {
-            label: 'Android',
-            url: '', // TODO: add url to playstore
-        },
-        {
-            label: 'iOS (soon)',
-            url: '', // TODO: add url to playstore
-        },
-    ]
+        const downloads = [
+            {
+                label: 'Android',
+                url: '', // TODO: add url to playstore
+            },
+            {
+                label: 'iOS (soon)',
+                url: '', // TODO: add url to playstore
+            },
+        ]
 
-    return (
-        <footer
-            ref={ref}
-            className={classNames(
-                'absolute inset-x-0 top-full z-50 flex min-h-screen flex-col space-y-8 bg-white px-4 py-2 md:px-8 md:py-2',
-                className,
-            )}
-            {...props}
-        >
-            <section className="flex flex-row justify-between">
-                <div className="space-y-8">
-                    <div className="ml-[-1rem] inline-flex items-center">
-                        <figure className="flex size-16 items-center justify-center md:size-24">
-                            <TheLogo />
-                        </figure>
-                        <h1 className="ml-[-0.5rem] text-4xl font-bold md:text-6xl">
-                            {appName}
-                        </h1>
+        return (
+            <footer
+                ref={ref}
+                className={classNames(
+                    'absolute inset-x-0 top-full z-50 flex min-h-screen flex-col space-y-8 bg-white px-4 py-2 md:px-8 md:py-2',
+                    className,
+                )}
+                {...props}
+            >
+                <section className="flex flex-row justify-between">
+                    <div className="space-y-8">
+                        <div className="ml-[-1rem] inline-flex items-center">
+                            <figure className="flex size-16 items-center justify-center md:size-24">
+                                <TheLogo />
+                            </figure>
+                            <h1 className="ml-[-0.5rem] text-4xl font-bold md:text-6xl">
+                                {appName}
+                            </h1>
+                        </div>
+                        <p className="text-lg md:text-2xl">{appDescription}</p>
                     </div>
-                    <p className="text-lg md:text-2xl">{appDescription}</p>
-                </div>
-            </section>
+                </section>
 
-            <section className="grid grid-cols-2 gap-y-2 md:gap-y-4">
-                <nav className="flex flex-col justify-start gap-y-2 md:gap-y-4">
-                    <h2 className="text-lg font-semibold md:text-2xl">
-                        Navigation
-                    </h2>
+                <section className="grid grid-cols-2 gap-y-2 md:gap-y-4">
+                    <nav className="flex flex-col justify-start gap-y-2 md:gap-y-4">
+                        <h2 className="text-lg font-semibold md:text-2xl">
+                            Navigation
+                        </h2>
 
-                    <ul className="flex flex-col gap-y-2 md:gap-y-4">
-                        {navigations.map(({ label, url }) => (
-                            <li key={label}>
-                                <a
-                                    className="text-lg hover:underline md:text-2xl"
-                                    href={url}
-                                >
-                                    {label}
-                                </a>
-                            </li>
-                        ))}
-                    </ul>
-                </nav>
-                <nav className="flex flex-col justify-start gap-y-2 md:gap-y-4">
-                    <h2 className="text-lg font-semibold md:text-2xl">
-                        Download
-                    </h2>
+                        <ul className="flex flex-col gap-y-2 md:gap-y-4">
+                            {navigations.map(({ label, url }) => (
+                                <li key={label}>
+                                    <a
+                                        className="text-lg hover:underline md:text-2xl"
+                                        href={url}
+                                    >
+                                        {label}
+                                    </a>
+                                </li>
+                            ))}
+                        </ul>
+                    </nav>
+                    <nav className="flex flex-col justify-start gap-y-2 md:gap-y-4">
+                        <h2 className="text-lg font-semibold md:text-2xl">
+                            Download
+                        </h2>
 
-                    <ul className="flex flex-col gap-y-2 md:gap-y-4">
-                        {downloads.map(({ label, url }) => (
-                            <li key={label}>
-                                <a
-                                    className="text-lg hover:underline md:text-2xl"
-                                    href={url}
-                                >
-                                    {label}
-                                </a>
-                            </li>
-                        ))}
-                    </ul>
-                </nav>
-            </section>
+                        <ul className="flex flex-col gap-y-2 md:gap-y-4">
+                            {downloads.map(({ label, url }) => (
+                                <li key={label}>
+                                    <a
+                                        className="text-lg hover:underline md:text-2xl"
+                                        href={url}
+                                    >
+                                        {label}
+                                    </a>
+                                </li>
+                            ))}
+                        </ul>
+                    </nav>
+                </section>
 
-            <hr className="border-1 border-black" />
+                <hr className="border-1 border-black" />
 
-            <section>
-                <div className="inline-flex items-center gap-x-1">
-                    <CIcon
-                        icon="lucide:copyright"
-                        className="text-xl md:text-2xl"
-                    />
-                    <p className="text-base md:text-lg">
-                        {moment().format('YYYY')} {appName} All Rights Reserved
-                    </p>
-                </div>
-            </section>
-        </footer>
-    )
-})
+                <section>
+                    <div className="inline-flex items-center gap-x-1">
+                        <CIcon
+                            icon="lucide:copyright"
+                            className="text-xl md:text-2xl"
+                        />
+                        <p className="text-base md:text-lg">
+                            {moment().format('YYYY')} {appName} All Rights
+                            Reserved
+                        </p>
+                    </div>
+                </section>
+            </footer>
+        )
+    },
+)
 
-export default LandingFooter
+export default Footer
--- src/components/landing/Header.tsx
+++ src/components/landing/Header.tsx
@@ -1,23 +1,18 @@
-import React, { forwardRef } from 'react'
 import TheLogo from '@/components/TheLogo'
 import classNames from 'classnames'
+import { ClassNameProps } from '@/types/componentTypes'
 
-const LandingHeader = forwardRef<
-    HTMLDivElement,
-    React.HTMLProps<HTMLDivElement>
->(({ className, ...props }, ref) => {
+type Props = ClassNameProps & {}
+
+const Header = ({ className }: Props) => {
     const appName = import.meta.env.VITE__APP_NAME
 
     return (
         <header
-            ref={ref}
             className={classNames(
-                // Base classes
                 'fixed inset-x-0 top-0 z-50 flex flex-row items-center border-b border-black/25 bg-white px-4 py-2 md:px-8',
-                // Incoming dynamic classes
                 className,
             )}
-            {...props}
         >
             <div className="inline-flex items-center">
                 <figure className="flex max-w-6 items-center justify-center md:max-w-8">
@@ -27,6 +22,6 @@ const LandingHeader = forwardRef<
             </div>
         </header>
     )
-})
+}
 
-export default LandingHeader
+export default Header
