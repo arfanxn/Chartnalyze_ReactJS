6f7df417e72466fca84c2e6b946f639be1db415e
Muhammad Arfan <arfan2173@gmail.com>
2025-05-01 21:46:27 +0700

    make button onclick is customable

--- src/components/CButton.tsx
+++ src/components/CButton.tsx
@@ -1,7 +1,11 @@
-import { ChildrenProps, ClassNameProps } from '@/types/componentTypes'
+import {
+    ChildrenProps,
+    ClassNameProps,
+    OnClickProps,
+} from '@/types/componentTypes'
 import classNames from 'classnames'
 
-type Props = ClassNameProps & ChildrenProps & {}
+type Props = ClassNameProps & ChildrenProps & OnClickProps & {}
 
 export default function CButton(props: Props) {
     return (
@@ -10,6 +14,7 @@ export default function CButton(props: Props) {
                 'bg-primary rounded-md px-2 py-1 text-white md:px-4 md:py-2',
                 props.className,
             )}
+            onClick={props.onClick}
         >
             {props.children}
         </button>
--- src/components/CButtonIconned.tsx
+++ src/components/CButtonIconned.tsx
@@ -1,14 +1,15 @@
 import CButton from '@/components/CButton'
 import CIcon from '@/components/CIcon'
-import { ClassNameProps } from '@/types/componentTypes'
+import { ClassNameProps, OnClickProps } from '@/types/componentTypes'
 import classNames from 'classnames'
 
-type Props = ClassNameProps & {
-    label: string
-    labelClassname?: string
-    icon: string
-    iconClassName?: string
-}
+type Props = ClassNameProps &
+    OnClickProps & {
+        label: string
+        labelClassname?: string
+        icon: string
+        iconClassName?: string
+    }
 
 export default function CButtonIconned(props: Props) {
     return (
@@ -17,6 +18,7 @@ export default function CButtonIconned(props: Props) {
                 'inline-flex items-center space-x-2',
                 props.className,
             )}
+            onClick={props.onClick}
         >
             <span className={classNames(props.labelClassname)}>
                 {props.label}
