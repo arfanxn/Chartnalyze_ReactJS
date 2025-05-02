42852e94e8eeb8f5d18ba439ca021fa89e83dcda
Muhammad Arfan <arfan2173@gmail.com>
2025-05-02 08:24:33 +0700

    added component types

--- src/types/componentTypes.ts
+++ src/types/componentTypes.ts
@@ -1,11 +1,40 @@
 import React, { ReactNode } from 'react'
 
+export type IdProps = {
+    id?: string
+}
+
 export type ClassNameProps = {
     className?: string
 }
 
+export type PlaceholderProps = {
+    placeholder?: string
+}
+
+export type OnClick = (event: React.MouseEvent<HTMLButtonElement>) => void
 export type OnClickProps = {
-    onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void
+    onClick?: OnClick
+}
+
+export type OnSubmit = (event: React.FormEvent<HTMLFormElement>) => void
+export type OnSubmitProps = {
+    onSubmit?: OnSubmit
+}
+
+export type OnInput = (event: React.FormEvent<HTMLInputElement>) => void
+export type OnInputProps = {
+    onInput?: OnInput
+}
+
+export type OnFocus = (event: React.FocusEvent<HTMLInputElement>) => void
+export type OnFocusProps = {
+    onFocus?: OnFocus
+}
+
+export type OnBlur = (event: React.FocusEvent<HTMLInputElement>) => void
+export type OnBlurProps = {
+    onBlur?: OnBlur
 }
 
 export type ChildrenProps = {
