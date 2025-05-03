b1258659255bf0936a5426b7d052ee5f7385f9d0
Muhammad Arfan <arfan2173@gmail.com>
2025-05-03 15:48:19 +0700

    created reusable alert

---
+++ src/components/CAlert.tsx
@@ -0,0 +1,32 @@
+import { forwardRef, HTMLAttributes } from 'react'
+import classNames from 'classnames'
+
+type Props = HTMLAttributes<HTMLDivElement> & {
+    messageClassName?: string
+    message: string
+    type: 'success' | 'info' | 'warning' | 'error'
+}
+
+const CAlert = forwardRef<HTMLDivElement, Props>(
+    ({ className, message, messageClassName, type, ...props }, ref) => (
+        <div ref={ref} className={classNames(className)} {...props}>
+            <p
+                className={classNames(
+                    {
+                        'text-primary': type === 'success',
+                        'text-blue-500': type === 'info',
+                        'text-yellow-500': type === 'warning',
+                        'text-secondary': type === 'error',
+                    },
+                    messageClassName,
+                )}
+            >
+                {message}
+            </p>
+        </div>
+    ),
+)
+
+CAlert.displayName = 'CAlert'
+
+export default CAlert
