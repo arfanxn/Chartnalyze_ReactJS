77f0cb2264771c97127e793ddb40798c49bcb33f
Muhammad Arfan <arfan2173@gmail.com>
2025-05-04 12:59:46 +0700

    renamed and updated

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+// index.tsx
+import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { StrictMode } from 'react'
+import App from '@/App'
+import store from '@/stores'
+
+createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+        <Provider store={store}>
+            <App></App>
+        </Provider>
+    </StrictMode>,
+)
