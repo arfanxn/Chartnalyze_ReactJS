d9b2f66ad3985889f00985b01f66bc2e69b8d4d6
Muhammad Arfan <arfan2173@gmail.com>
2025-05-04 13:57:28 +0700

    fixed bug here

--- src/components/CToast.tsx
+++ src/components/CToast.tsx
@@ -1,22 +1,39 @@
 import CAlert from '@/components/CAlert'
 import classNames from 'classnames'
 import { forwardRef, HTMLAttributes } from 'react'
+import { ToastContentProps } from 'react-toastify'
 
-type Props = HTMLAttributes<HTMLDivElement> & {
-    messageClassName?: string
-    message: string
-    type: 'success' | 'info' | 'warning' | 'error'
-}
+type Props = HTMLAttributes<HTMLDivElement> &
+    ToastContentProps & {
+        messageClassName?: string
+        message: string
+        type: 'success' | 'info' | 'warning' | 'error'
+    }
 
 const CToast = forwardRef<HTMLDivElement, Props>(
-    ({ className, message, messageClassName, type, ...props }, ref) => (
+    (
+        {
+            className,
+            message,
+            messageClassName,
+            type,
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            closeToast,
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            toastProps,
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            isPaused,
+            ...props
+        },
+        ref,
+    ) => (
         <CAlert
+            {...props}
             ref={ref}
             className={classNames(className)}
             message={message}
             type={type}
             messageClassName={classNames('font-semibold', messageClassName)}
-            {...props}
         />
     ),
 )
