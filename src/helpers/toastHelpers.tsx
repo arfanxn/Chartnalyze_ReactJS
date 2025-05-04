56b6995a18683dde42b86102a34b65cfc6ef84e5
Muhammad Arfan <arfan2173@gmail.com>
2025-05-04 14:02:22 +0700

    created toast helpers

---
+++ src/helpers/toastHelpers.tsx
@@ -0,0 +1,12 @@
+import { toast as _toast } from 'react-toastify'
+import CToast from '@/components/CToast'
+
+export const toast = ({
+    message,
+    type,
+}: {
+    message: string
+    type: 'success' | 'info' | 'warning' | 'error'
+}) => {
+    _toast(<CToast message={message} type={type} />)
+}
