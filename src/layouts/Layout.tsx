f619335060f429042a6b30440b71819e78e999f5
Muhammad Arfan <arfan2173@gmail.com>
2025-05-04 13:00:25 +0700

    updated main layout

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,40 +1,29 @@
-import { ChildrenProps, ClassNameProps } from '@/types/componentTypes'
 import classNames from 'classnames'
-import { ReactNode } from 'react'
+import { forwardRef, HTMLProps } from 'react'
 import { useCurrentRouteTitle } from '@/hooks/useCurrentRouteTitle'
 import { useScrollToTopOnRouteChange } from '@/hooks/useScrollToTopOnRouteChange'
 
-type Props = ClassNameProps & ChildrenProps & {}
+type Props = HTMLProps<HTMLElement>
 
-/**
- * Layout component that sets the document title based on route match titles.
- *
- * @param {Object} props - The props object.
- * @param {ReactNode} props.children - The child components to render.
- *
- * @returns {ReactNode} Rendered children components.
- *
- * This component uses the `useMatches` hook from `react-router` to get the
- * current route matches and filter them for those with a title handle.
- * It then sets the document title to the last matching route's title, followed
- * by the application name. If no route has a title handle, the document title
- * defaults to the application name.
- */
+const Layout = forwardRef<HTMLElement, Props>(
+    ({ className, ...props }, ref) => {
+        useCurrentRouteTitle()
+        useScrollToTopOnRouteChange()
 
-const Layout = (props: Props): ReactNode => {
-    useCurrentRouteTitle()
-    useScrollToTopOnRouteChange()
+        return (
+            <main
+                ref={ref}
+                className={classNames(
+                    'relative flex flex-col space-y-16 px-4 md:space-y-24 md:px-8',
+                    className,
+                )}
+            >
+                {props.children}
+            </main>
+        )
+    },
+)
 
-    return (
-        <main
-            className={classNames(
-                'relative flex flex-col space-y-16 px-4 md:space-y-24 md:px-8',
-                props.className,
-            )}
-        >
-            {props.children}
-        </main>
-    )
-}
+Layout.displayName = 'Layout'
 
 export default Layout
