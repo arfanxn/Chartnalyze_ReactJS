d78c02cf5517d7d650fa7873f39ea55274872dd0
Muhammad Arfan <arfan2173@gmail.com>
2025-05-11 13:36:16 +0700

    registered app slice

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,12 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit'
+import appReducer from '@/stores/appSlice'
 import userReducer from '@/stores/userSlice'
 
 const store = configureStore({
     reducer: {
+        app: appReducer,
         user: userReducer,
     },
 })
 
+export type AppDispatch = typeof store.dispatch
 export type RootState = ReturnType<typeof store.getState>
 
 export default store
