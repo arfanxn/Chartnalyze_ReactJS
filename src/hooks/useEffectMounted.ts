5b62a417b06af08587463eeaa3d56bbed38c9ef2
Muhammad Arfan <arfan2173@gmail.com>
2025-05-11 17:04:56 +0700

    added doc string

--- src/hooks/useEffectMounted.ts
+++ src/hooks/useEffectMounted.ts
@@ -1,5 +1,11 @@
 import { useEffect, useRef } from 'react'
 
+/**
+ * Custom hook that runs the provided effect only once when the component is mounted.
+ *
+ * @param effect - A function containing the effect to run on mount. It can return a cleanup function.
+ */
+
 export const useEffectMounted = (effect: () => void | (() => void)) => {
     const isMounted = useRef<boolean>(false)
 
