cf1920317defa8f122b1f6674cb6cf8b5bbe8a23
Muhammad Arfan <arfan2173@gmail.com>
2025-05-11 14:27:12 +0700

    added isNumber and isString functions

--- src/utils/booleanUtils.ts
+++ src/utils/booleanUtils.ts
@@ -1,1 +1,7 @@
 export const isEven = (num: number): boolean => num % 2 === 0
+
+export const isNumber = (value: unknown): value is number =>
+    typeof value === 'number' && isFinite(value)
+
+export const isString = (value: unknown): value is string =>
+    typeof value === 'string'
