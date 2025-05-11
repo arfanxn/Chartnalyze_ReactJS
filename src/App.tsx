b1ecc22a34fe75bfcb75942cfb235eabbecffc66
Muhammad Arfan <arfan2173@gmail.com>
2025-05-11 18:15:47 +0700

    rollback App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,12 @@
 // App.tsx
 import '@/assets/styles/index.css'
-import { useEffectMounted } from './hooks/useEffectMounted'
 import { RouterProvider } from 'react-router'
-import { setSelf } from './stores/userSlice'
-import { showSelf } from '@/services/userService'
-import { Suspense, useCallback } from 'react'
+import { Suspense, useEffect } from 'react'
 import CToastContainer from '@/components/CToastContainer'
 import { router } from '@/router'
-import { useAppDispatch } from './hooks/useAppDispatch'
 
 const App = () => {
-    const dispatch = useAppDispatch()
-
-    const loadSelf = useCallback(async () => {
-        try {
-            const { user } = await showSelf()
-            dispatch(setSelf(user))
-            // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        } catch (error) {
-            //
-        }
-    }, [dispatch])
-
-    useEffectMounted(() => {
-        loadSelf()
-    })
+    useEffect(() => {}, [])
 
     return (
         <Suspense fallback={<div>Loading...</div>}>
