bc1e21fd8fada48460084fc95ef36c4384af2ade
Muhammad Arfan <arfan2173@gmail.com>
2025-05-11 17:05:16 +0700

    initialization in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
 // App.tsx
 import '@/assets/styles/index.css'
+import { useEffectMounted } from './hooks/useEffectMounted'
 import { RouterProvider } from 'react-router'
-import { Suspense } from 'react'
+import { setSelf } from './stores/userSlice'
+import { showSelf } from '@/services/userService'
+import { Suspense, useCallback } from 'react'
 import CToastContainer from '@/components/CToastContainer'
 import { router } from '@/router'
+import { useAppDispatch } from './hooks/useAppDispatch'
 
 const App = () => {
+    const dispatch = useAppDispatch()
+
+    const loadSelf = useCallback(async () => {
+        try {
+            const { user } = await showSelf()
+            dispatch(setSelf(user))
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        } catch (error) {
+            //
+        }
+    }, [dispatch])
+
+    useEffectMounted(() => {
+        loadSelf()
+    })
+
     return (
         <Suspense fallback={<div>Loading...</div>}>
             <RouterProvider router={router} />
