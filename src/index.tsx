2d11a8eac0f346b455864a9723d86fb507cdc506
Muhammad Arfan <arfan2173@gmail.com>
2025-05-11 17:42:46 +0700

    disabled StrictMode

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 // index.tsx
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
 import App from '@/App'
 import store from '@/stores'
 
 createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <Provider store={store}>
-            <App></App>
-        </Provider>
-    </StrictMode>,
+    // <StrictMode>
+    <Provider store={store}>
+        <App></App>
+    </Provider>,
+    // </StrictMode>,
 )
