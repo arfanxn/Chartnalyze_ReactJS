ba1abc3157589088570783a48273c9692eff33ea
Muhammad Arfan <arfan2173@gmail.com>
2025-05-12 13:01:22 +0700

    removed redux zzzzz

--- src/hooks/useAppSelector.ts
+++
@@ -1,4 +0,0 @@
-import type { RootState } from '@/stores'
-import { TypedUseSelectorHook, useSelector } from 'react-redux'
-
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
