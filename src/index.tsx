23f0908c33f76de42d8b25f8b09ce34c9a01c95f
Muhammad Arfan <arfan2173@gmail.com>
2025-05-12 13:05:00 +0700

    removed redux zzzzz

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,10 @@
 // index.tsx
 import { createRoot } from 'react-dom/client'
-import { Provider } from 'react-redux'
 // import { StrictMode } from 'react'
 import App from '@/App'
-import store from '@/stores'
 
 createRoot(document.getElementById('root')!).render(
     // <StrictMode>
-    <Provider store={store}>
-        <App></App>
-    </Provider>,
+    <App></App>,
     // </StrictMode>,
 )
