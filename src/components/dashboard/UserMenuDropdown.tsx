b54ff33f79c0e79e01581401a689f439975795e2
Muhammad Arfan <arfan2173@gmail.com>
2025-05-14 01:06:45 +0700

    created functioning user menu dropdown

---
+++ src/components/dashboard/UserMenuDropdown.tsx
@@ -0,0 +1,101 @@
+import { forwardRef, HTMLAttributes } from 'react'
+import classNames from 'classnames'
+import { useSelfStore } from '@/stores/useSelfStore'
+import { Link, useNavigate } from 'react-router'
+import CIcon from '@/components/CIcon'
+import CDropdown from '@/components/CDropdown'
+import * as userService from '@/services/userService'
+import { toast } from '@/helpers/toastHelpers'
+import axios from 'axios'
+
+type Props = HTMLAttributes<HTMLDivElement>
+
+const NAVIGATIONS = [
+    {
+        label: 'Profile',
+        icon: 'lucide:circle-user-round',
+        url: '/users/self/profile',
+    },
+    {
+        label: 'Settings',
+        icon: 'lucide:settings',
+        url: '/users/self/settings',
+    },
+]
+
+const UserMenuDropdown = forwardRef<HTMLDivElement, Props>(
+    ({ className, ...props }, ref) => {
+        const navigate = useNavigate()
+
+        const self = useSelfStore((state) => state.self)
+        const setSelf = useSelfStore((state) => state.setSelf)
+        if (self === null) return null
+
+        const logoutAction = async () => {
+            // TODO: implement a better UI confirmation dialog
+            if (confirm('Are you sure you want to logout?') === false) return
+
+            try {
+                const { message } = await userService.logout()
+                setSelf(null)
+                toast({ message, type: 'success' })
+                navigate('/login', { replace: true })
+            } catch (e) {
+                if (axios.isAxiosError(e)) {
+                    toast({ message: e.response!.data.message, type: 'error' })
+                }
+                throw e
+            }
+        }
+
+        return (
+            <CDropdown
+                ref={ref}
+                className={classNames('w-48 md:w-64', className)}
+                {...props}
+            >
+                <header className="space-y-2">
+                    <h3 className="text-sm leading-none font-medium text-black md:text-base">
+                        {self.name}
+                    </h3>
+                    <p className="text-sm leading-none font-light text-black">
+                        {self.email}
+                    </p>
+                </header>
+
+                <nav className="mt-4 space-y-2">
+                    {NAVIGATIONS.map(({ label, icon, url }) => (
+                        <Link
+                            key={url}
+                            className={classNames(
+                                'flex w-full items-center gap-x-2 rounded-md px-2 py-1 font-medium text-black transition-colors duration-150 hover:bg-neutral-100 hover:text-black md:text-base',
+                            )}
+                            to={url}
+                        >
+                            <CIcon icon={icon} className="text-xl" />
+                            {label}
+                        </Link>
+                    ))}
+                </nav>
+
+                <hr className="border-0.5 border-neutral-300" />
+
+                <footer>
+                    <button
+                        className={classNames(
+                            'flex w-full items-center gap-x-2 rounded-md px-2 py-1 font-medium text-black transition-colors duration-150 hover:bg-neutral-100 hover:text-black md:text-base',
+                        )}
+                        onClick={logoutAction}
+                    >
+                        <CIcon icon="lucide:log-out" className="text-xl" />
+                        <span>Logout</span>
+                    </button>
+                </footer>
+            </CDropdown>
+        )
+    },
+)
+
+UserMenuDropdown.displayName = 'UserMenuDropdown'
+
+export default UserMenuDropdown
