4fde840242b223faf6d3ff50517844c2de6c8e0d
Muhammad Arfan <arfan2173@gmail.com>
2025-05-15 21:36:03 +0700

    defined form types

---
+++ src/types/formTypes/index.tsx
@@ -0,0 +1,7 @@
+/* eslint-disable react-refresh/only-export-components */
+
+// User form types
+export * from '@/types/formTypes/userFormTypes'
+
+// OTP form types
+export * from '@/types/formTypes/otpFormTypes'
