364191589381c7fa6c3c054ffb4f3dc53b818fbc
Muhammad Arfan <arfan2173@gmail.com>
2025-05-15 21:36:25 +0700

    used form types

--- src/repositories/userRepository.ts
+++ src/repositories/userRepository.ts
@@ -2,23 +2,27 @@ import axiosInstance from '@/lib/axios'
 import { AxiosResponse } from 'axios'
 import { ResponseBody, ResponseBodyData } from '@/types/responseTypes'
 import { User } from '@/types/userTypes'
+import {
+    LoginForm,
+    OtpCodeForm,
+    RegisterForm,
+    UpdateSelfEmailForm,
+    UpdateSelfForm,
+    UpdateSelfPasswordForm,
+} from '@/types/formTypes'
 
 // TODO: Save user data into local storage or other solution is to fetch user data from backend on every page reload
 
-export const register = async (form: {
-    username: string
-    email: string
-    password: string
-    confirmPassword: string
-}): Promise<AxiosResponse<ResponseBodyData<{ user: User }>>> => {
+export const register = async (
+    form: RegisterForm,
+): Promise<AxiosResponse<ResponseBodyData<{ user: User }>>> => {
     const response = await axiosInstance.post(`/api/users/register`, form)
     return response
 }
 
-export const login = async (form: {
-    identifier: string // username or email
-    password: string
-}): Promise<AxiosResponse<ResponseBodyData<{ accessToken: string }>>> => {
+export const login = async (
+    form: LoginForm,
+): Promise<AxiosResponse<ResponseBodyData<{ accessToken: string }>>> => {
     const response = await axiosInstance.post(`/api/users/login`, form)
     return response
 }
@@ -28,9 +32,9 @@ export const logout = async (): Promise<AxiosResponse<ResponseBody>> => {
     return response
 }
 
-export const verify = async (form: {
-    code: string
-}): Promise<AxiosResponse<ResponseBody>> => {
+export const verify = async (
+    form: OtpCodeForm,
+): Promise<AxiosResponse<ResponseBody>> => {
     const response = await axiosInstance.post(
         `/api/users/self/email/verify`,
         form,
@@ -42,5 +46,26 @@ export const showSelf = async (): Promise<
     AxiosResponse<ResponseBodyData<{ user: User }>>
 > => {
     const response = await axiosInstance.get(`/api/users/self`)
+    return response
+}
+
+export const updateSelf = async (
+    form: UpdateSelfForm,
+): Promise<AxiosResponse<ResponseBodyData<{ user: User }>>> => {
+    const response = await axiosInstance.put(`/api/users/self`, form)
+    return response
+}
+
+export const updateSelfEmail = async (
+    form: UpdateSelfEmailForm,
+): Promise<AxiosResponse<ResponseBodyData<{ user: User }>>> => {
+    const response = await axiosInstance.patch(`/api/users/self/email`, form)
+    return response
+}
+
+export const updateSelfPassword = async (
+    form: UpdateSelfPasswordForm,
+): Promise<AxiosResponse<ResponseBody>> => {
+    const response = await axiosInstance.put(`/api/users/self/email`, form)
     return response
 }
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,13 +1,18 @@
 import Cookies from 'js-cookie'
 import { User } from '@/types/userTypes'
 import * as userRepository from '@/repositories/userRepository'
+import {
+    LoginForm,
+    OtpCodeForm,
+    RegisterForm,
+    UpdateSelfEmailForm,
+    UpdateSelfForm,
+    UpdateSelfPasswordForm,
+} from '@/types/formTypes'
 
-export const register = async (form: {
-    username: string
-    email: string
-    password: string
-    confirmPassword: string
-}): Promise<{ message: string; user: User }> => {
+export const register = async (
+    form: RegisterForm,
+): Promise<{ message: string; user: User }> => {
     const response = await userRepository.register(form)
     return {
         message: response.data.message,
@@ -15,10 +20,7 @@ export const register = async (form: {
     }
 }
 
-export const login = async (form: {
-    identifier: string // username or email
-    password: string
-}): Promise<{ message: string }> => {
+export const login = async (form: LoginForm): Promise<{ message: string }> => {
     const response = await userRepository.login(form)
     const { accessToken } = response.data.data
     Cookies.set('access_token', accessToken, {
@@ -38,9 +40,9 @@ export const logout = async (): Promise<{ message: string }> => {
     }
 }
 
-export const verify = async (form: {
-    code: string
-}): Promise<{ message: string }> => {
+export const verify = async (
+    form: OtpCodeForm,
+): Promise<{ message: string }> => {
     const response = await userRepository.verify(form)
     return {
         message: response.data.message,
@@ -52,5 +54,28 @@ export const showSelf = async () => {
     return {
         message: response.data.message,
         user: response.data.data.user,
+    }
+}
+
+export const updateSelf = async (form: UpdateSelfForm) => {
+    const response = await userRepository.updateSelf(form)
+    return {
+        message: response.data.message,
+        user: response.data.data.user,
+    }
+}
+
+export const updateSelfEmail = async (form: UpdateSelfEmailForm) => {
+    const response = await userRepository.updateSelfEmail(form)
+    return {
+        message: response.data.message,
+        user: response.data.data.user,
+    }
+}
+
+export const updateSelfPassword = async (form: UpdateSelfPasswordForm) => {
+    const response = await userRepository.updateSelfPassword(form)
+    return {
+        message: response.data.message,
     }
 }
