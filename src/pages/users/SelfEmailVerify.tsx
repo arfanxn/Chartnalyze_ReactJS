76a0ea29fad3ac87a989948591379054a9230790
Muhammad Arfan <arfan2173@gmail.com>
2025-05-16 16:50:45 +0700

    remvoed unused comment

--- src/pages/users/SelfEmailVerify.tsx
+++ src/pages/users/SelfEmailVerify.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react-hooks/rules-of-hooks */
 import { useNavigate } from 'react-router'
 import { toast } from '@/helpers/toastHelpers'
 import EntryLayout from '@/layouts/EntryLayout'
