6a2a70da74e7ede5345aa65aa4325046edca3fd6
Muhammad Arfan <arfan2173@gmail.com>
2025-05-16 16:51:34 +0700

    created SelfEmailEdit

---
+++ src/pages/users/SelfEmailEdit.tsx
@@ -0,0 +1,35 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import { useNavigate } from 'react-router'
+import { toast } from '@/helpers/toastHelpers'
+import EntryLayout from '@/layouts/EntryLayout'
+import { useSelfStore } from '@/stores/useSelfStore'
+import VerifyCard from '@/components/users/VerifyCard'
+import { useLocation } from 'react-router'
+
+function SelfEmailEdit() {
+    const { search } = useLocation()
+    const query = new URLSearchParams(search)
+    const email = query.get('email')
+    if (!email) return null
+    const navigate = useNavigate()
+
+    const updateSelfEmail = useSelfStore((state) => state.updateEmail)
+
+    const handle = async (form: { email: string; code: number }) => {
+        const { message } = await updateSelfEmail(form)
+        toast({ message, type: 'success' })
+        navigate('/users/self/profile/edit', { replace: true })
+    }
+
+    return (
+        <EntryLayout>
+            <section className="flex min-h-[100vh] flex-row items-center justify-center">
+                <VerifyCard email={email} onVerify={handle} />
+            </section>
+        </EntryLayout>
+    )
+}
+
+SelfEmailEdit.displayName = 'SelfEmailEdit'
+
+export default SelfEmailEdit
