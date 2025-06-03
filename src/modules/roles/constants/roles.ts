b2d035e2e4e35913a877c46133fb725539d2b02b
Muhammad Arfan <arfan2173@gmail.com>
2025-06-03 22:49:13 +0700

    created role  types

--- src/modules/roles/constants/roles.ts
+++ src/modules/roles/constants/roles.ts
@@ -1,3 +1,7 @@
 import { RoleName } from '@/modules/roles/types/RoleName'
 
-export const ROLES: RoleName[] = ['admin', 'analyst', 'user']
+export const ROLE_ADMIN: RoleName = 'admin'
+export const ROLE_ANALYST: RoleName = 'analyst'
+export const ROLE_USER: RoleName = 'user'
+
+export const ROLE_NAMES: RoleName[] = [ROLE_ADMIN, ROLE_ANALYST, ROLE_USER]
