7685cfbe3a4b1c55771d9c2a9cda418d3f3e5f2d
Muhammad Arfan <arfan2173@gmail.com>
2025-06-03 23:05:28 +0700

    updated LoadingKeys

--- src/core/types/loadingKeys.ts
+++ src/core/types/loadingKeys.ts
@@ -1,9 +1,20 @@
 export type LoadingKeys =
     // User
     | 'self'
+    | 'users.index'
+    | 'users.show'
+    | 'users.update'
+    | 'users.destroy'
 
     // Notifications
     | 'notifications.index'
     | 'notifications.toggleRead'
+
+    // Roles
+    | 'roles.index'
+    | 'roles.assignUser'
+
+    // Permissions
+    | 'permissions.index'
 
 // there could be more
