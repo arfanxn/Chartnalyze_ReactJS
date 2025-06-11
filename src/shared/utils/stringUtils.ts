4f056bcd89d20247945a6f00ce28afd9d4b66e04
Muhammad Arfan <arfan2173@gmail.com>
2025-06-11 23:34:58 +0700

    created `snakeCaseToTitleCase`

--- src/shared/utils/stringUtils.ts
+++ src/shared/utils/stringUtils.ts
@@ -1,3 +1,10 @@
 export const uppercaseFirst = (str: string): string => {
     return str.charAt(0).toUpperCase() + str.slice(1)
 }
+
+export const snakeCaseToTitleCase = (str: string): string => {
+    return str
+        .split('_')
+        .map((word) => uppercaseFirst(word))
+        .join(' ')
+}
